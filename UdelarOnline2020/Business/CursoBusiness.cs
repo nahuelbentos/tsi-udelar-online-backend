dc9dfe82b09bc1d766ecbd02d632ecfd6f1bdbe5
Tomas Pascal <tomas.pascal@fing.edu.uy>
2020-10-14 18:43:42 -0300

    Avances WebAPI, Business

---
+++ UdelarOnline2020/Business/CursoBusiness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Models;
+using Persistence;
+
+namespace Business
+{
+    public class CursoBusiness
+    {
+        private readonly Contexto _context;
+
+        public CursoBusiness()
+        {
+            _context = new Contexto();
+        }
+
+        public Curso GetCurso(int id)
+        {
+            return _context.Curso.Find(id);
+        }
+    }
+}
