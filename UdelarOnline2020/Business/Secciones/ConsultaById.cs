1db9c84bfd81833ca9943a4b6828bcaca6cf150f
Alberto del Pino <dpalbertt@gmail.com>
2020-11-10 13:54:20 -0300

    CRUD Secciones v1. Falta probar

---
+++ UdelarOnline2020/Business/Secciones/ConsultaById.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.ManejadorError;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Persistence;
+
+namespace Business.Secciones {
+    public class ConsultaById
+    {
+        public class Ejecuta : IRequest<Seccion>
+        {
+            public Guid SeccionId { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, Seccion>
+        {
+            private readonly UdelarOnlineContext context;
+
+            public Manejador(UdelarOnlineContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Seccion> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                //Hay que reemplazarlo con DataTypes
+                var carrera = await this.context.Seccion
+                                                    .FirstOrDefaultAsync(c => c.SeccionId == request.SeccionId);
+                if (carrera == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.Forbidden, new { mensaje = "No existe una seccion con el SeccionId ingresado" });
+                }
+                return carrera;
+            }
+        }
+    }
+}
