5bf9e71339f4b5d0aad5f568f50a85886b176eb8
Alberto del Pino <dpalbertt@gmail.com>
2020-11-11 20:18:56 -0300

    Testeado ABM Seccion. Agrego colecciones de Postman

---
+++ UdelarOnline2020/WebAPI/Controllers/SeccionController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Business.Secciones;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeccionController : MiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<Unit>> AltaSeccion(Nuevo.Ejecuta data)
+        {
+            return await this.Mediator.Send(data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Seccion>>> GetSecciones()
+        {
+            return await this.Mediator.Send(new Consulta.Ejecuta());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Seccion>> GetSeccion(Guid id)
+        {
+            return await this.Mediator.Send(new ConsultaById.Ejecuta { SeccionId = id });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> ModificarSeccion(Guid Id, Editar.Ejecuta data)
+        {
+            data.SeccionId = Id;
+            return await this.Mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> EliminarSeccion(Guid Id)
+        {
+            return await this.Mediator.Send(new Eliminar.Ejecuta { SeccionId = Id });
+        }
+        
+    }
+}
