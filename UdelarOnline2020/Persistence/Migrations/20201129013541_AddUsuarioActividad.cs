7b8c1ce62b326cba2051d539f0ac89b0d21e5c16
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-11-28 22:36:45 -0300

    Agrego migracion para agregar usuario que crea la actividad  a actividades

---
+++ UdelarOnline2020/Persistence/Migrations/20201129013541_AddUsuarioActividad.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddUsuarioActividad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Actividad",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actividad_UsuarioId",
+                table: "Actividad",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actividad_AspNetUsers_UsuarioId",
+                table: "Actividad",
+                column: "UsuarioId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actividad_AspNetUsers_UsuarioId",
+                table: "Actividad");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Actividad_UsuarioId",
+                table: "Actividad");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Actividad");
+        }
+    }
+}
