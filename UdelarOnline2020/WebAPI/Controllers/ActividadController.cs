006824a6575b1dda6d90ed27391bbceb3c98db06
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-11-29 03:22:47 -0300

    Add editarEncuesta

--- UdelarOnline2020/WebAPI/Controllers/ActividadController.cs
+++ UdelarOnline2020/WebAPI/Controllers/ActividadController.cs
@@ -10,39 +10,50 @@ using Models;
 
 namespace WebAPI.Controllers
 {
-    
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActividadController : MiControllerBase
+
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ActividadController : MiControllerBase
+  {
+    [HttpPost]
+    public async Task<ActionResult<Unit>> AltaActividad(Nuevo.Ejecuta data) => await this.Mediator.Send(data);
+
+    [HttpPost("pruebaonline")]
+    public async Task<ActionResult<Unit>> AltaPruebaOnline(NuevaPruebaOnline.Ejecuta data) => await this.Mediator.Send(data);
+
+    [HttpGet]
+    public async Task<ActionResult<List<Actividad>>> GetActividades() => await this.Mediator.Send(new Consulta.Ejecuta());
+    [HttpGet("tipo/{tipo}")]
+    public async Task<ActionResult<List<Actividad>>> GetActividadesByTipo(string tipo) => await this.Mediator.Send(new ConsultaByTipo.Ejecuta { Tipo = tipo });
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Actividad>> GetActividad(Guid id) => await this.Mediator.Send(new ConsultaById.Ejecuta { ActividadId = id });
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> ModificarActividad(Guid id, Editar.Ejecuta data)
     {
-        [HttpPost]
-        public async Task<ActionResult<Unit>> AltaActividad(Nuevo.Ejecuta data) => await this.Mediator.Send(data);
-
-        [HttpPost("encuesta")]
-        public async Task<ActionResult<Unit>> AltaEncuesta(NuevaEncuesta.Ejecuta data) => await this.Mediator.Send(data);
-
-        [HttpPost("pruebaonline")]
-        public async Task<ActionResult<Unit>> AltaPruebaOnline(NuevaPruebaOnline.Ejecuta data) => await this.Mediator.Send(data);
-
-        [HttpGet]
-        public async Task<ActionResult<List<Actividad>>> GetActividades() => await this.Mediator.Send(new Consulta.Ejecuta());
-        [HttpGet("tipo/{tipo}")]
-        public async Task<ActionResult<List<Actividad>>> GetActividadesByTipo( string tipo) => await this.Mediator.Send(new ConsultaByTipo.Ejecuta{ Tipo = tipo });
-
-        [HttpGet("encuesta/{id}")]
-        public async Task<ActionResult<DtEncuesta>> GetEncuesta(Guid id) => await this.Mediator.Send(new ConsultaEncuestaById.Ejecuta { Id = id});
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Actividad>> GetActividad(Guid id) => await this.Mediator.Send(new ConsultaById.Ejecuta { ActividadId = id});
-
-        [HttpPut("{id}")]
-        public async Task<ActionResult<Unit>> ModificarActividad(Guid id, Editar.Ejecuta data)
-        {
-            data.ActividadId = id;
-            return await this.Mediator.Send(data);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> EliminarActividad(Guid id) => await this.Mediator.Send(new Eliminar.Ejecuta { ActividadId = id});
+      data.ActividadId = id;
+      return await this.Mediator.Send(data);
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Unit>> EliminarActividad(Guid id) => await this.Mediator.Send(new Eliminar.Ejecuta { ActividadId = id });
+
+
+    // Encuesta
+
+    [HttpGet("encuesta/{id}")]
+    public async Task<ActionResult<DtEncuesta>> GetEncuesta(Guid id) => await this.Mediator.Send(new ConsultaEncuestaById.Ejecuta { Id = id });
+
+    [HttpPost("encuesta")]
+    public async Task<ActionResult<Unit>> AltaEncuesta(NuevaEncuesta.Ejecuta data) => await this.Mediator.Send(data);
+
+    [HttpPut("encuesta/{id}")]
+    public async Task<ActionResult<Unit>> ModificarEncuesta(Guid id, Editar.Ejecuta data)
+    {
+      data.ActividadId = id;
+      return await this.Mediator.Send(data);
+    }
+  }
 }
